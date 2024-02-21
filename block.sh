966209b387291015f44ba7e40c9d983bcf488135
林千里 <lincheney@gmail.com>
2024-02-21 19:52:30 +1100

    upper case the block

--- block.sh
+++ block.sh
@@ -1,4 +1,4 @@
-alias @with_block='{ block.call() { __block_set_func'
+alias @with_block='{ BLOCK.call() { __block_set_func'
 # __block_open='[;'
 # __block_close=';]'
 alias ']'='}; __block_run; }'
@@ -10,8 +10,8 @@ __block_set_func() {
 }
 
 __block_run() {
-    eval "set -- $(block.call)"
-    eval "$(declare -f block.call | sed '/__block_set_func /{ d; q }')"
+    eval "set -- $(BLOCK.call)"
+    eval "$(declare -f BLOCK.call | sed '/__block_set_func /{ d; q }')"
     "$@"
 }
 
@@ -21,7 +21,7 @@ __block_run() {
 __block__make-block() {
     local funcname="__block-$1"
     # save a new function
-    eval "$(declare -f block.call | sed "1s/^block\\.call/$funcname/")"
+    eval "$(declare -f BLOCK.call | sed "1s/^BLOCK\\.call/$funcname/")"
     alias "@$1=@with_block $funcname"
 }
 alias @make-block='@with_block __block__make-block'
@@ -37,14 +37,14 @@ fi
 @make-block map [
     __failed=0
     while __block_read_array __args; do
-        block.call "${__args[@]}" || __failed=1
+        BLOCK.call "${__args[@]}" || __failed=1
     done
     (( !__failed ))
 ]
 
 @make-block filter [
     while __block_read_array __args; do
-        if block.call "${__args[@]}"; then
+        if BLOCK.call "${__args[@]}"; then
             printf '%s\n' "${__args[*]}"
         fi
     done
